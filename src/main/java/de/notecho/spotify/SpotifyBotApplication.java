a3b4d79186050e77775c6cc97a586c9d3715d42d
janhz <jan@notecho.de>
2022-01-11 23:45:10 +0100

    Initializing Spring Boot project with gradle

---
+++ src/main/java/de/notecho/spotify/SpotifyBotApplication.java
@@ -0,0 +1,13 @@
+package de.notecho.spotify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/notecho/spotify/SpotifyBotApplicationTests.java
@@ -0,0 +1,13 @@
+package de.notecho.spotify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
