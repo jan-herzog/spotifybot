85875bbb029d2561e9390263c2ccb636c4d49687
janherzog <jan@notecho.de>
2022-01-19 08:39:09 +0100

    Login/Logout features

--- src/main/java/de/notecho/spotify/web/controller/LoginController.java
+++ src/main/java/de/notecho/spotify/web/controller/LoginController.java
@@ -1,20 +1,29 @@
 package de.notecho.spotify.web.controller;
 
+import de.notecho.spotify.web.session.SessionManagementService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
 public class LoginController {
 
+    private final SessionManagementService sessionManagementService;
+
     @GetMapping("/login")
-    public String login(Model model) {
-        return "index";
+    public String login(@CookieValue(name = "session", defaultValue = "null") String session, Model model) {
+        if(session.equals("null"))
+            return "redirect:https://id.twitch.tv/oauth2/authorize?client_id=41cemr1kitmxxmv15o47v152b78xzr&redirect_uri=https://spitchbot.com/twitch/callback/&response_type=code&scope=user:read:email user_read";
+        return "redirect:/dashboard";
     }
 
     @GetMapping("/logout")
     public String logout(Model model) {
-        return "index";
+        return "redirect:/";
     }
 
 }
