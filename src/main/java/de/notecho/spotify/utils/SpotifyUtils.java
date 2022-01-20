3ecf58c8aa3e34d39502974558c468a89eea2255
janherzog <jan@notecho.de>
2022-01-20 13:42:12 +0100

    added `SpotifyUtils`

---
+++ src/main/java/de/notecho/spotify/utils/SpotifyUtils.java
@@ -0,0 +1,31 @@
+package de.notecho.spotify.utils;
+
+
+import com.google.gson.JsonParser;
+import com.wrapper.spotify.model_objects.specification.ArtistSimplified;
+import com.wrapper.spotify.model_objects.specification.Track;
+import se.michaelthelin.spotify.model_objects.specification.ArtistSimplified;
+import se.michaelthelin.spotify.model_objects.specification.Track;
+
+public class SpotifyUtils {
+
+    public static String getArtists(Track track) {
+        StringBuilder artists = new StringBuilder();
+        for (ArtistSimplified artist : track.getArtists()) {
+            if (artists.toString().equalsIgnoreCase("")) {
+                artists.append(artist.getName());
+                continue;
+            }
+            artists.append(", ").append(artist.getName());
+        }
+        return artists.toString();
+    }
+
+    public static String getUriFromJson(String json) {
+        return new JsonParser().parse(json).getAsJsonObject().get("item").getAsJsonObject().get("uri").getAsString();
+    }
+
+    public static String getIdFromUri(String uri) {
+        return uri.replace("spotify:track:", "");
+    }
+}
